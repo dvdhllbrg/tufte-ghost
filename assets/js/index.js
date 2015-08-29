19a91f09b096fea15f35def7a978db43b4c4b9d5
David Hallberg Jönsson <d@vidhallberg.se>
2015-08-29 21:31:03 +0200

    Add function for placing footnotes

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,44 +13,30 @@
         var $postContent = $(".post-content");
         $postContent.fitVids();
 
-        $(".scroll-down").arctic_scroll();
-
         $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
             e.preventDefault();
             $("body").toggleClass("nav-opened nav-closed");
         });
 
+        setTimeout(placeFootnotes, 1);
+
+        $(window).resize(placeFootnotes);
+
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
-
-    };
 })(jQuery);
+
+var placeFootnotes = function() {
+    if($('.footnotes').length > 0) {
+        var numberOfFootnotes = $('.footnotes ol li').last().attr('id').substring($('.footnotes ol li').last().attr('id').indexOf(':')+1);
+        var width = $(window).width();
+
+        if(width > 760) {
+            for(var i=1; i<=numberOfFootnotes; i++) {
+                var top = Math.floor($('#fnref\\:' + i).position().top);
+                $('#fn\\:' + i).css('top', (top - 24) + 'px');
+            }
+            $('a[href^="#fnref"]').remove();
+        }
+    }
+}
